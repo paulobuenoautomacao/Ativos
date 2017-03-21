4e1bb044e1e1e7af2ab4f5747a5b2b87bebdd209
Paulo Bueno <paulobuenoautomacao@gmail.com>
2017-03-21 20:22:36 -0300

    Início de desenvolvimento do core (Módulo base do sistema)

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company, Subsidiary, Department, Local, Equipment, Employee, Manufacturer
+
+class CompanyAdmin(admin.ModelAdmin):
+	list_display = ['name', 'created', 'modified']
+	search_fields = ['name', 'created', 'modified']
+
+class SubsidiaryAdmin(admin.ModelAdmin):
+	list_display = ['code', 'name', 'company', 'created', 'modified']
+	search_fields = ['code', 'name', 'company__name', 'created', 'modified']
+
+class DepartmentAdmin(admin.ModelAdmin):
+	list_display = ['code', 'name', 'subsidiary', 'created', 'modified']
+	search_fields = ['code', 'name', 'subsidiary__name', 'created', 'modified']
+
+class LocalAdmin(admin.ModelAdmin):
+	list_display = ['code', 'name', 'department', 'created', 'modified']
+	search_fields = ['code', 'name', 'department__name', 'created', 'modified']
+
+class EquipmentAdmin(admin.ModelAdmin):
+	list_display = ['code', 'name', 'local', 'created', 'modified']
+	search_fields = ['code', 'name', 'local__name', 'created', 'modified']
+
+class EmployeeAdmin(admin.ModelAdmin):
+	list_display = ['code', 'name', 'department', 'created', 'modified']
+	search_fields = ['code', 'name', 'department__name', 'created', 'modified']
+
+class ManufacturerAdmin(admin.ModelAdmin):
+	list_display = ['name', 'company', 'created', 'modified']
+	search_fields = ['name', 'company__name', 'created', 'modified']
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Subsidiary, SubsidiaryAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Local, LocalAdmin)
+admin.site.register(Equipment, EquipmentAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Manufacturer, ManufacturerAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,118 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+
+	name = models.CharField('Nome', max_length=100)
+	description	= models.TextField('Descrição', blank=True)
+	created	= models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Compania'
+		verbose_name_plural = 'Companias'
+		ordering = ['name']
+
+
+class Subsidiary(models.Model):
+
+	code = models.CharField('Código', max_length=4)
+	name = models.CharField('Nome', max_length=100)
+	description	= models.TextField('Descrição', blank=True)
+	company = models.ForeignKey('Company', on_delete=models.CASCADE, verbose_name='Compania')
+	created	= models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__(self):
+		return '%s %s' % (self.code, self.name)
+
+	class Meta:
+		verbose_name = 'Unidade'
+		verbose_name_plural = 'Unidades'
+		ordering = ['code']
+
+class Department(models.Model):
+
+	code = models.CharField('Código', max_length=2)
+	name = models.CharField('Nome', max_length=100)
+	description	= models.TextField('Descrição', blank=True)
+	subsidiary = models.ForeignKey('Subsidiary', on_delete=models.CASCADE, verbose_name='Unidade')
+	created	= models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__(self):
+		return '%s %s' % (self.code, self.name)
+
+	class Meta:
+		verbose_name = 'Departamento'
+		verbose_name_plural = 'Departamentos'
+		ordering = ['code']
+
+class Local(models.Model):
+
+	code = models.CharField('Código', max_length=5)
+	name = models.CharField('Nome', max_length=100)
+	description	= models.TextField('Descrição', blank=True)
+	department = models.ForeignKey('Department', on_delete=models.CASCADE, verbose_name='Departamento')
+	created	= models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__(self):
+		return '%s %s' % (self.code, self.name)
+
+	class Meta:
+		verbose_name = 'Local'
+		verbose_name_plural = 'Locais'
+		ordering = ['code']
+
+class Equipment(models.Model):
+
+	code = models.CharField('Código', max_length=10)
+	name = models.CharField('Nome', max_length=100)
+	description	= models.TextField('Descrição', blank=True)
+	local = models.ForeignKey('Local',  on_delete=models.CASCADE, verbose_name='Local')
+	created	= models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__(self):
+		return '%s %s' % (self.code, self.name)
+
+	class Meta:
+		verbose_name = 'Equipamento'
+		verbose_name_plural = 'Equipamentos'
+		ordering = ['code']
+
+class Employee(models.Model):
+
+	code = models.CharField('Código', max_length=10)
+	name = models.CharField('Nome', max_length=100)
+	position = models.TextField('Cargo', blank=True)
+	department = models.ForeignKey('Department', on_delete=models.CASCADE, verbose_name='Departamento')
+	created	= models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__(self):
+		return '%s %s' % (self.code, self.name)
+
+	class Meta:
+		verbose_name = 'Funcionário'
+		verbose_name_plural = 'Funcionários'
+		ordering = ['name']
+
+class Manufacturer(models.Model):
+
+	name = models.CharField('Nome', max_length=100)
+	description	= models.TextField('Descrição', blank=True)
+	company = models.ForeignKey('Company', on_delete=models.CASCADE, verbose_name='Compania')
+	created	= models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__(self):
+		return self.name
+		
+	class Meta:
+		verbose_name = 'Fabricante'
+		verbose_name_plural = 'Fabricantes'
+		ordering = ['name']
