cf2bb80c600894b20c0765d029ffffdb744817a9
Paulo Bueno <paulobuenoautomacao@gmail.com>
2017-03-25 23:17:21 -0300

    Criado arquivos csv para importação de dados. Dados continuidade no desenvolvimento do módulo Asset (Gestão de Ativos).

--- asset/admin.py
+++ asset/admin.py
@@ -1,25 +1,45 @@
 from django.contrib import admin
 
-from .models import Asset, Tag, AssetType, Repair
+from .models import Asset, AssetTag, AssetType, Criticalness, ControlMesh, ControlMeshTag
 
-class AssetAdmin(admin.ModelAdmin):
-	list_display = ['model', 'patrimony', 'manufacturer', 'equipment', 'created', 'modified']
-	search_fields = ['model', 'patrimony', 'manufacturer__name', 'equipment__name', 'created', 'modified']
-	filter_horizontal = ['repair']
-
-class TagAdmin(admin.ModelAdmin):
+class AssetTagAdmin(admin.ModelAdmin):
 	list_display = ['name', 'tag', 'asset', 'created', 'modified']
 	search_fields = ['name', 'tag', 'asset__model', 'created', 'modified']
+
+class AssetTagInline(admin.StackedInline):
+    model = AssetTag
+    extra = 1
+
+class AssetAdmin(admin.ModelAdmin):
+	list_display = ['model', 'equipment', 'description']
+	search_fields = ['model', 'patrimony', 'manufacturer__name', 'equipment__name', 'created', 'modified']
+	inlines = [AssetTagInline]
 
 class AssetTypeAdmin(admin.ModelAdmin):
 	list_display = ['code', 'name', 'created', 'modified']
 	search_fields = ['code', 'name', 'created', 'modified']
 
-class RepairAdmin(admin.ModelAdmin):
-	list_display = ['name', 'manufacturer', 'created', 'modified']
-	search_fields = ['name', 'manufacturer__name', 'created', 'modified']
+class CriticalnessAdmin(admin.ModelAdmin):
+	list_display = ['code', 'name', 'created', 'modified']
+	search_fields = ['code', 'name', 'created', 'modified']
+
+class ControlMeshTagAdmin(admin.ModelAdmin):
+	list_display = ['name', 'tag', 'controlmesh', 'created', 'modified']
+	search_fields = ['name', 'tag', 'controlmesh__name', 'created', 'modified']
+
+class ControlMeshTagInline(admin.StackedInline):
+    model = ControlMeshTag
+    extra = 1
+
+class ControlMeshAdmin(admin.ModelAdmin):
+	list_display = ['name', 'assetType', 'equipment', 'created', 'modified']
+	search_fields = ['name', 'assetType__name', 'equipment__name' 'created', 'modified']
+	filter_horizontal = ['asset']
+	inlines = [ControlMeshTagInline]
 
 admin.site.register(Asset, AssetAdmin)
-admin.site.register(Tag, TagAdmin)
+admin.site.register(AssetTag, AssetTagAdmin)
 admin.site.register(AssetType, AssetTypeAdmin)
-admin.site.register(Repair, RepairAdmin)
+admin.site.register(Criticalness, CriticalnessAdmin)
+admin.site.register(ControlMesh, ControlMeshAdmin) 
+admin.site.register(ControlMeshTag, ControlMeshTagAdmin)
--- asset/models.py
+++ asset/models.py
@@ -7,9 +7,9 @@ class Asset(models.Model):
 	assetType = models.ForeignKey('AssetType', on_delete=models.PROTECT, verbose_name='Tipo de ativo')
 	patrimony = models.CharField('Patrimônio', max_length=20, blank=True)
 	description	= models.TextField('Descrição', blank=True)
-	manufacturer = models.ForeignKey('core.Manufacturer', on_delete=models.PROTECT, verbose_name='Fabricante')
-	equipment = models.ForeignKey('core.Equipment', verbose_name='Equipamento')
-	repair = models.ManyToManyField('Repair', verbose_name='Reparos', blank=True)
+	manufacturer = models.ForeignKey('core.Manufacturer', on_delete=models.PROTECT, verbose_name='Fabricante', null=True, blank=True)
+	equipment = models.ForeignKey('core.Equipment', on_delete=models.PROTECT, verbose_name='Equipamento')
+	criticalness = models.ForeignKey('Criticalness', on_delete=models.PROTECT, verbose_name='Criticidade', null=True, blank=True)
 	created	= models.DateTimeField('Criado em', auto_now_add=True)
 	modified = models.DateTimeField('Modificado em', auto_now=True)
 
@@ -17,15 +17,15 @@ class Asset(models.Model):
 		return self.model
 
 	class Meta:
-		verbose_name = 'Equipamento'
-		verbose_name_plural = 'Equipamentos'
-		ordering = ['equipment']
+		verbose_name = 'Ativo'
+		verbose_name_plural = 'Ativos'
+		ordering = ['equipment', 'assetType']
 
-class Tag(models.Model):
+class AssetTag(models.Model):
 
 	name = models.CharField('Nome', max_length=100)
 	tag = models.CharField('TAG', max_length=50)
-	asset = models.ForeignKey('Asset', on_delete=models.CASCADE, verbose_name='Equipamento')
+	asset = models.ForeignKey('Asset', on_delete=models.CASCADE, verbose_name='Ativo')
 	created	= models.DateTimeField('Criado em', auto_now_add=True)
 	modified = models.DateTimeField('Modificado em', auto_now=True)
 
@@ -33,8 +33,8 @@ class Tag(models.Model):
 		return '%s %s' % (self.name, self.tag)
 
 	class Meta:
-		verbose_name = 'TAG'
-		verbose_name_plural = 'TAGs'
+		verbose_name = 'TAG do Ativo'
+		verbose_name_plural = 'TAGs dos Ativos'
 		ordering = ['tag']
 
 class AssetType(models.Model):
@@ -53,11 +53,11 @@ class AssetType(models.Model):
 		verbose_name_plural = 'Tipos de Ativos'
 		ordering = ['name']
 
-class Repair(models.Model):
+class Criticalness(models.Model):
 
+	code = models.CharField('Código', max_length=4)
 	name = models.CharField('Nome', max_length=100)
 	description	= models.TextField('Descrição', blank=True)
-	manufacturer = models.ForeignKey('core.Manufacturer', on_delete=models.PROTECT, verbose_name='Fabricante')
 	created	= models.DateTimeField('Criado em', auto_now_add=True)
 	modified = models.DateTimeField('Modificado em', auto_now=True)
 
@@ -65,6 +65,41 @@ class Repair(models.Model):
 		return self.name
 
 	class Meta:
-		verbose_name = 'Reparo'
-		verbose_name_plural = 'Reparos'
-		ordering = ['name']
+		verbose_name = 'Criticidade'
+		verbose_name_plural = 'Criticidades'
+		ordering = ['code']
+
+class ControlMesh(models.Model):
+
+	name = models.CharField('Nome', max_length=100)
+	description	= models.TextField('Descrição', blank=True)
+	assetType = models.ForeignKey('AssetType', on_delete=models.PROTECT, verbose_name='Tipo de malha')
+	equipment = models.ForeignKey('core.Equipment', on_delete=models.PROTECT, verbose_name='Equipamento')
+	criticalness = models.ForeignKey('Criticalness', on_delete=models.PROTECT, verbose_name='Criticidade', blank=True)
+	asset = models.ManyToManyField('Asset', verbose_name='Ativos', blank=True)
+	created	= models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Malha de controle'
+		verbose_name_plural = 'Malhas de controle'
+		ordering = ['equipment']
+
+class ControlMeshTag(models.Model):
+
+	name = models.CharField('Nome', max_length=100)
+	tag = models.CharField('TAG', max_length=50)
+	controlmesh = models.ForeignKey('ControlMesh', on_delete=models.CASCADE, verbose_name='Malha de controle')
+	created	= models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__(self):
+		return '%s %s' % (self.name, self.tag)
+
+	class Meta:
+		verbose_name = 'TAG da Malha de controle'
+		verbose_name_plural = 'TAGs das Malhas de controle'
+		ordering = ['tag']
